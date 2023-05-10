92e7777da1c2deb6bce37f76bcd92801db2f8ccc
Sofité <sofiatrajanyweb@gmail.com>
2023-05-10 09:59:18 -0300

    js

--- Proyect Ed Visto/JS/main.js
+++ Proyect Ed Visto/JS/main.js
@@ -1,1 +1,18 @@
 console.log("hola");
+
+const urlYouTube = document.getElementById ('youtubeLink');
+const validateButton = document.getElementById ('checkCircle');
+const messageError = document.getElementById ('resultadoErrorLink');
+
+validateButton.addEventListener('click', checkCircle()); {
+  const urlRegex = /^(https?\:\/\/)?(www\.youtube\.com|youtu\.?be)\/.+$/;
+  const url = urlYouTube.value.trim();
+
+  if (urlRegex.test(url)) {
+    resultMessage.innerText = 'Valid YouTube URL';
+    resultMessage.classList.remove('error');
+  } else {
+    resultMessage.innerText = 'Invalid YouTube URL';
+    resultMessage.classList.add('error');
+  }
+};
